5e8abac3362327dd43992f3cefebbbaa73e39658
hai <lminhhai12@gmail.com>
2026-07-12 11:15:05 +0700

    Mô tả thay đổi

--- js/home.js
+++ js/home.js
@@ -1,3 +1,10 @@
+import { auth } from "./firebase.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const homeBtn = document.querySelector('#homeBtn')
 const registerBtn = document.querySelector('#registerBtn')
 const loginBtn = document.querySelector('#loginBtn')
@@ -5,58 +12,86 @@ const playlistBtn = document.querySelector('#playlistBtn')
 const chartsBtn = document.querySelector('#chartsBtn')
 const videoBtn = document.querySelector('#videoBtn')
 
-homeBtn.addEventListener('click', () => {
-    window.location.href = 'home.html'
-})
+if(homeBtn){
+    homeBtn.addEventListener('click', () => {
+        window.location.href = 'home.html'
+    })
+}
 
-registerBtn.addEventListener('click', () => {
-    window.location.href = 'register/register.html'
-})
+if(registerBtn){
+    registerBtn.addEventListener('click', () => {
+        window.location.href = 'register/register.html'
+    })
+}
 
-loginBtn.addEventListener('click', () => {
-    window.location.href = 'login/login.html'
-})
+if(loginBtn){
+    loginBtn.addEventListener('click', () => {
+        window.location.href = 'login/login.html'
+    })
+}
 
-playlistBtn.addEventListener('click', () => {
-    window.location.href = 'playlist/playlist.html'
-})
-chartsBtn.addEventListener('click', () => {
-    window.location.href = 'BXH.html'
-})
-videoBtn.addEventListener('click', () => {
-    window.location.href = 'Video.html'
-})
+if(playlistBtn){
+    playlistBtn.addEventListener('click', () => {
+        window.location.href = 'playlist/playlist.html'
+    })
+}
+
+if(chartsBtn){
+    chartsBtn.addEventListener('click', () => {
+        window.location.href = 'BXH.html'
+    })
+}
+if(videoBtn){
+    videoBtn.addEventListener('click', () => {
+        window.location.href = 'Video.html'
+    })
+}
 let index = 0
 
 const slides = document.querySelector('.slides')
-const total = slides.children.length
 
-function showSlide(i){
 
-    index = i
+if(slides){
 
-    if(index >= total){
-        index = 0
+    const total = slides.children.length
+
+
+    function showSlide(i){
+
+        index = i
+
+        if(index >= total){
+            index = 0
+        }
+
+        if(index < 0){
+            index = total - 1
+        }
+
+        slides.style.transform =
+        `translateX(-${index * 100}%)`
+
     }
 
-    if(index < 0){
-        index = total - 1
+
+    document.querySelector('.next').onclick = () => {
+        showSlide(index + 1)
     }
 
-    slides.style.transform = `translateX(-${index * 100}%)`
+
+    document.querySelector('.prev').onclick = () => {
+        showSlide(index - 1)
+    }
+
+
+    setInterval(() => {
+        showSlide(index + 1)
+    },3000)
+
 }
 
-document.querySelector('.next').onclick = () => {
-    showSlide(index + 1)
-}
 
-document.querySelector('.prev').onclick = () => {
-    showSlide(index - 1)
-}
 
-setInterval(() => {
-    showSlide(index + 1)
-}, 3000)
 
 
 const playBtn = document.querySelector('#playBtn')
@@ -64,20 +99,123 @@ const audioPlayer = document.querySelector('#audioPlayer')
 
 let isPlaying = false
 
-playBtn.addEventListener('click', () => {
 
-    if(isPlaying){
+if(playBtn){
 
-        audioPlayer.pause()
-        playBtn.innerHTML = '▶'
+    playBtn.addEventListener('click',()=>{
 
-    }
-    else{
 
-        audioPlayer.play()
-        playBtn.innerHTML = '⏸'
+        if(isPlaying){
 
-    }
+            audioPlayer.pause()
 
-    isPlaying = !isPlaying
-})
+            playBtn.innerHTML = '▶'
+
+        }
+
+        else{
+
+            audioPlayer.play()
+
+            playBtn.innerHTML = '⏸'
+
+        }
+
+
+        isPlaying = !isPlaying
+
+    })
+
+}
+
+const accountArea = document.querySelector("#accountArea")
+
+
+if(accountArea){
+
+    onAuthStateChanged(auth,(user)=>{
+
+
+        if(user){
+
+
+            accountArea.innerHTML = `
+
+            <span>
+                ${user.email}
+            </span>
+
+            <button id="logoutBtn">
+                Đăng xuất
+            </button>
+
+            `
+
+
+
+            document
+            .querySelector("#logoutBtn")
+            .onclick = () => {
+
+
+                signOut(auth)
+                .then(()=>{
+
+                    alert("Đã đăng xuất")
+
+                    location.reload()
+
+                })
+
+
+            }
+
+
+
+        }
+
+        else{
+
+
+            accountArea.innerHTML = `
+
+            <button id="registerBtn">
+                Đăng ký
+            </button>
+
+
+            <button id="loginBtn">
+                Đăng nhập
+            </button>
+
+            `
+
+
+
+            document
+            .querySelector("#registerBtn")
+            .onclick = () => {
+
+                window.location.href =
+                "register/register.html"
+
+            }
+
+
+
+            document
+            .querySelector("#loginBtn")
+            .onclick = () => {
+
+                window.location.href =
+                "login/login.html"
+
+            }
+
+
+        }
+
+
+    })
+
+}
